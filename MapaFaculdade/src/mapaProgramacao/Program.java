db9455b46bb9cf9150e4e4b98df2c2dbf5ba55d2
jccesar22 <60848753+jccesar22@users.noreply.github.com>
2021-08-26 22:08:21 -0300

    Commit
    Finalizando o sistema da faculdade

--- MapaFaculdade/src/mapaProgramacao/Program.java
+++ MapaFaculdade/src/mapaProgramacao/Program.java
@@ -3,10 +3,8 @@ package mapaProgramacao;
 
 
 import java.util.Scanner;
-
 import cadastro.Produto;
 import cadastro.Relatorio;
-import console.Console;
 import edicaoPreco.EdicaoPreco;
 import movimentoDeEstoque.MovimentoEntrada;
 
@@ -21,7 +19,6 @@ public class Program {
 			System.out.println("**************************************MAMU**************************************");
 			System.out.println("1 - CADASTRO DE PRODUTOS\n" + "2 - MOVIMENTAÇÃO\n" + "3 - REAJUSTE DE PREÇOS\n"
 					+ "4 - RELATÓRIOS\n" + "0 - FINALIZAR\n" + "OPÇÃO: _");
-			
 			 op = teclado1.nextInt();
 			switch (op) {
 			case 1:
@@ -40,13 +37,17 @@ public class Program {
 				System.out.println("opcão 4");
 				Relatorio.relatorio();
 				break;
+			case 0:
+				System.out.println("Finalizando o sistema..............100%");
+				break;
 			default:
 				System.out.println("Digite uma opcao valida");
 			break;
 			}
 
 		}
-		while (op != 0);
+		while (op != 0); 
+		System.out.println("Sistema finalizado");
 	}
 
 	
